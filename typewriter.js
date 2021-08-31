e7f5c3a5fb8c1fdeb6e2152576eca61208857b28
Maria-Lavinia Otelea <otelea.lavi@gmail.com>
2021-08-31 14:30:13 +0200

    making it work without the extras

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,25 @@
 "use strict";
+
+let numberOfIterations;
+let iterator;
+let character;
+let text = document.querySelector("#typewriter").textContent;
+document.querySelector("#typewriter").textContent="";
+console.log(text);
+
+init();
+
+function init() {
+  numberOfIterations = text.length;
+  iterator = 0;
+  loop();
+}
+function loop() {
+  console.log("loop", iterator);
+character = text[iterator];
+iterator++;
+document.querySelector("#typewriter").textContent += character;
+  if (iterator < numberOfIterations) {
+    setTimeout(loop, 500);
+  }
+}
